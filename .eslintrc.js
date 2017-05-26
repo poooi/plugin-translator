0156c4c70e4e73b4efea1facf17f62f17b2f1d96
Kagami <moe.pascal@gmail.com>
2017-05-26 23:17:25 +0800

    add eslintrc

---
+++ .eslintrc.js
@@ -0,0 +1,47 @@
+module.exports = {
+  'env': {
+    'browser': true,
+    'es6': true,
+    'node': true,
+  },
+  'extends': [
+    "airbnb-base",
+  ],
+  'parser': 'babel-eslint',
+  'globals': {
+    'config': false,
+  },
+  'rules': {
+    'comma-dangle': ['error', 'always-multiline'],
+    'semi': ['error', 'never'],
+    'no-underscore-dangle': 'off',
+    'consistent-return': 'off',
+  },
+  'settings': {
+    'import/resolver': {
+      'node': {
+        'extensions': ['.js', '.jsx', '.es', '.coffee', '.cjsx'],
+        'paths': [__dirname],
+      },
+    },
+    'import/core-modules': [
+      'bluebird',
+      'electron',
+      'react',
+      'react-redux',
+      'redux-observers',
+      'reselect',
+      'react-bootstrap',
+      'react-fontawesome',
+      'path-extra',
+      'fs-extra',
+      'lodash',
+      'cson',
+      'react-dom',
+      'redux',
+      'semver',
+      'i18n-2',
+      'glob',
+    ],
+  },
+}
