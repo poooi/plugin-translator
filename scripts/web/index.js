4432c8bb32be602eb211056ba44fe52bb0a57636
Kagami <moe.pascal@gmail.com>
2018-11-20 00:35:11 +0800

    fix: full width numbers

--- scripts/web/index.js
+++ scripts/web/index.js
@@ -4,6 +4,8 @@ import { outputJson } from 'fs-extra'
 import path from 'path'
 import fetch from 'node-fetch'
 
+const fullWidth = fromPairs(map(['０', '１', '２', '３', '４', '５', '６', '７', '８', '９'], (n, i) => [i, n]))
+
 const getUpdateFromWikiaPage = async () => {
   const resp = await fetch('http://kancolle.wikia.com/wiki/Quests')
   const html = await resp.text()
@@ -12,7 +14,7 @@ const getUpdateFromWikiaPage = async () => {
   const rows = dom.window.document.querySelectorAll('tr[class*=quest]:not([class*=details])')
   const items = map(rows, row =>
     [
-      trim(row.querySelector('span[lang=ja').textContent).replace(/!/g, '！'),
+      trim(row.querySelector('span[lang=ja').textContent).replace(/!/g, '！').replace(/[0-9]/g, match => fullWidth[match]),
       trim(row.querySelector('i').textContent),
     ])
 
