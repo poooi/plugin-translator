7736f6acf832574bb0a77f3e6e4840002f8d2b70
Kagami <moe.pascal@gmail.com>
2018-11-29 00:00:42 +0800

    chore: simplify eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,57 +1,24 @@
 module.exports = {
-  'env': {
-    'browser': true,
-    'es6': true,
-    'node': true,
+  env: {
+    browser: true,
+    es6: true,
+    node: true,
   },
-  'extends': [
-    "airbnb-base",
-    "poi-plugin",
-  ],
-  'parser': 'babel-eslint',
-  'globals': {
-    'config': false,
-  },
-  'rules': {
+  extends: ['airbnb-base', 'poi-plugin'],
+  parser: 'babel-eslint',
+  rules: {
     'comma-dangle': ['error', 'always-multiline'],
-    'semi': ['error', 'never'],
+    semi: ['error', 'never'],
     'no-underscore-dangle': 'off',
     'consistent-return': 'off',
-  },
-  'settings': {
-    'import/resolver': {
-      'node': {
-        'extensions': ['.js', '.jsx', '.es', '.coffee', '.cjsx'],
-        'paths': [__dirname],
-      },
-    },
-    'import/core-modules': [
-      'bluebird',
-      'electron',
-      'react',
-      'react-redux',
-      'redux-observers',
-      'reselect',
-      'react-bootstrap',
-      'react-fontawesome',
-      'path-extra',
-      'fs-extra',
-      'lodash',
-      'cson',
-      'react-dom',
-      'redux',
-      'semver',
-      'i18n-2',
-      'glob',
-    ],
   },
   overrides: [
     {
       files: ['scripts/**/*.js', 'update.js'],
       rules: {
-        'import/no-extraneous-dependencies': ['error', { 'devDependencies': true }],
+        'import/no-extraneous-dependencies': ['error', { devDependencies: true }],
         'no-console': 'off',
-      }
-    }
+      },
+    },
   ],
 }
